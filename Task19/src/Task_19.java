2323cfa2696d133a1cf3299040d96f74d2b1590d
DimKopatich <dimakash71020111@gmail.com>
2023-06-14 13:34:05 +0300

    Task19

---
+++ Task19/src/Task_19.java
@@ -0,0 +1,30 @@
+// Даны два числа. Определить цифры, входящие в запись как первого так и второго числа.
+import java.util.Scanner;
+
+public class Task_19 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        String a = scanner.nextLine();
+        System.out.println("Введите число b: ");
+        String b = scanner.nextLine();
+
+        for (int f = 0; f < a.length(); f++) {
+            a.charAt(f);
+        }
+        System.out.println(" ");
+        for (int g = 0; g < b.length(); g++) {
+            b.charAt(g);
+        }
+        System.out.println("Цифры, входящие в оба числа: ");
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    System.out.print("|" + b.charAt(j));
+                }
+            }
+        }
+    }
+}
